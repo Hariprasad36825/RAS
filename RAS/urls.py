98d60a6107ab281e36d241ab834e35926e509f55
Hari Prasad <hariprasad36825@gmail.com>
2021-05-22 18:01:05 +0530

    8

--- RAS/urls.py
+++ RAS/urls.py
@@ -27,6 +27,6 @@ router.register(r'login', views.LoginView, 'Login')
 urlpatterns = [
     path('', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/', include('Restaurant.urls'))
+    path('api/', include('Restaurant.urls')),
     re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
